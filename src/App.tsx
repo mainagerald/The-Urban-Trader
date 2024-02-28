e6eb7f319deb90dee171f1132cb0aa8d8dac0476
mainagerald <flavianmaina2401@gmail.com>
2024-02-28 15:58:11 +0300

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import { CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./CoDataTypes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface SearchResponse {
+    data: CompanySearch[];
+};
 
-export default App;
+export const searchCompanies = async (query:string) => {
+    console.log('started');
+    try{
+        const data = await axios.get<SearchResponse>(
+            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=JQ4cRTJbMx5CmzVicDYb1vcYeA5cG4cw`
+        );
+        // console.log(data);
+        return data;
+    }
+    catch (error){
+        if(axios.isAxiosError(error)){
+            console.log("error: ", error.message)
+            return error.message;
+        }else{
+            console.log("unexpected error: ", error);
+            return "Unexpected error occurred!"
+        }
+    }
+};
+
+export const getCompanyProflie = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyProfile[]>(`https://financialmodelingprep.com/api/v3/profile/${query}?apikey=JQ4cRTJbMx5CmzVicDYb1vcYeA5cG4cw`)
+
+        return data;
+    } catch (error: any){
+        console.log("Unexpected error: ", error.message)
+    }
+};
+
+export const getKeyMetrics = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyKeyMetrics[]>(`https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=JQ4cRTJbMx5CmzVicDYb1vcYeA5cG4cw`)
+
+        return data;
+    } catch (error: any){
+        console.log("Unexpected error: ", error.message)
+    }
+};
+
+export const getIncomeStatement = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyIncomeStatement[]>(`https://financialmodelingprep.com/api/v3/income-statement/${query}?apikey=JQ4cRTJbMx5CmzVicDYb1vcYeA5cG4cw`)
+
+        return data;
+    } catch (error: any){
+        console.log("Unexpected error: ", error.message)
+    }
+};
+
